a95b373caf27a1453e4eee3028170a3f0398d464
IKA TI21f <114545046+ikasalbella@users.noreply.github.com>
2022-12-01 16:33:38 +0700

    Add files via upload

---
+++ UTS ESSAY/BangunDatar.java
@@ -0,0 +1,16 @@
+
+ // Nama    : IKA
+ // NIM     : 20210040096
+ // Kelas   : TI21F
+
+package BangunDatar;
+
+/**
+ *
+ * @author User
+ */
+    public interface BangunDatar {
+    void luas();
+    double getLuas();
+}
+
